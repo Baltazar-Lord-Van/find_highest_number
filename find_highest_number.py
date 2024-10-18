4637e23155d0749ef8c743c6c3340563da1bac2b
Van <lodvan22@gmail.com>
2024-10-18 19:57:27 +0800

    Added input validation

--- find_highest_number.py
+++ find_highest_number.py
@@ -2,8 +2,12 @@ highest_number = None
 
 for i in range(5):
     user_input = input(f"Enter number{i + 1}: ")
-    number = float(user_input)
-
+    try:
+        number = float(user_input)
+    except ValueError:
+        print("Invalid input! Please enter a number.")
+        continue    
+    
     if highest_number is None:
         highest_number = number
     if number > highest_number:
